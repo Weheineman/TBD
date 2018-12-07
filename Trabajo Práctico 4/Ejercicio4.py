6aa35cd9f3f3ae5d43f2e93aedebd5778a47a863
Sebastián Moliné <38443011+SebastianMoline@users.noreply.github.com>
2018-12-07 18:01:06 -0300

    Funciones comentadas

--- Trabajo Práctico 4/Ejercicio4.py
+++ Trabajo Práctico 4/Ejercicio4.py
@@ -1,5 +1,6 @@
+#Dado alfa y F, devuelve el cierre de alfa.
 def cierreAlfa(alfa, F):
-	resultado = set(alfa)
+	resultado = alfa
 	cantidad = -1
 	nuevaCantidad = len(resultado)
 	while(cantidad != nuevaCantidad):
@@ -11,20 +12,42 @@ def cierreAlfa(alfa, F):
 	return resultado
 
 
-def partes(lista):
-	if lista == []: 
+#Dadas dos listas de conjuntos (parcialChico y parcialGrande), devuelve una 
+#lista con los elementos de parcialChico y los elementos de parcialGrande 
+#que no incluyen a ningún elemento de parcialChico.
+def noContenidos(parcialChico, parcialGrande):
+	resultado = []
+	for conjunto in parcialGrande:
+		flag = False
+		for subconjunto in parcialChico:
+			if(subconjunto <= conjunto):
+				flag = True
+				break
+		
+		if(not flag):
+			resultado = resultado + [conjunto]
+	return resultado + parcialChico
+
+
+#Dado un conjunto de dependencias, la cantidad de atributos, un conjunto base 
+#y un conjunto de atributos, calcula el conjunto de claves posibles.
+def calcularClaves(dependencias, cantAtributos, base, atributos):
+	if(atributos == []):
 		return []
-	head = lista[0]
-	subconjuntos  = partes(lista[1:])
-	subconjuntos2 = [ [head] + x for x in subconjuntos]
-	return [[head]] + subconjuntos + subconjuntos2	
+	
+	posibleClave = base.copy()
+	posibleClave.add(atributos[0])
+	if(len(cierreAlfa(posibleClave, dependencias)) == cantAtributos):
+		return [posibleClave] + calcularClaves(dependencias, cantAtributos, base, atributos[1:])
+	
+	parcial1 = calcularClaves(dependencias, cantAtributos, base, atributos[1:])
+	parcial2 = calcularClaves(dependencias, cantAtributos, posibleClave, atributos[1:])
+
+	return noContenidos(parcial1, parcial2)
 
 
-def filtrar(cant, conjunto):
-	return [x for x in conjunto if len(x) == cant]
-
-
-def clavesCandidatas(R, F):
+#Dado R y F, devuelve el conjunto de claves candidatas, de forma no ordenada.	
+def clavesCandidatasAux(R, F):
 	cantAtributos = len(R)
 	dependencias = [(set(x), set(y)) for (x,y) in F]
 	basica = set(R)
@@ -32,51 +55,54 @@ def clavesCandidatas(R, F):
 		basica = basica - y
 	
 	clavePosible = basica
-	largoClavePosible = len(clavePosible)
-	cierreClavePosible = cierreAlfa(clavePosible, dependencias)
-	largoCierrePosible = len(cierreClavePosible)
-	if(largoCierrePosible == cantAtributos):
+	cierrePosible = cierreAlfa(clavePosible, dependencias)
+	if(len(cierrePosible) == cantAtributos):
 		resultado = [list(clavePosible)]
-	else:					
-		restantes = set(R) - cierreClavePosible
-		resultado = []
-		cantExtra = 0
-		while (restantes != set() and cantExtra <= cantAtributos - largoCierrePosible):
-			cantExtra += 1
-			posibles = map(set, filtrar(cantExtra, partes(list(restantes))))
-			for extra in posibles:
-				intento = basica | extra
-				if(len(cierreAlfa(intento, dependencias)) == cantAtributos):
-					resultado = resultado + [list(intento)]
-					restantes = restantes - extra
+	else:
+		restantes = [x for x in R if x not in cierrePosible]
+		resultado = calcularClaves(dependencias, cantAtributos, clavePosible, restantes)
+		resultado = list(map(list, resultado))
 		
 	return resultado
 
+
+#Dado R y F, devuelve el conjunto de claves candidatas, de forma ordenada.	
+def clavesCandidatas(R, F):
+	return(sorted(map(sorted, clavesCandidatasAux(R, F))))
+		
 
 # ~ Set3
 # ~ ARGUMENTOS:
 ERE3 = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
 EFE3 = [ (['A'], ['I']), (['A', 'B'],['C']), (['A', 'D'],['G', 'H']), (['B', 'D'],['E', 'F']), (['H'],['J']) ]
 # ~ RESULTADO OBTENIDO
-# ~ CLAVES = [['D', 'A', 'B']]
+# ~ CLAVES = [['A', 'B', 'D']]
 
 # ~ Set4
 # ~ ARGUMENTOS:
 ERE4 = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
 EFE4 = [ (['A'], ['B', 'C']), (['C'],['D']), (['D'],['G']), (['E', 'A']), (['E', 'H']), (['H'], ['E']) ]
 # ~ RESULTADO OBTENIDO
-# ~ CLAVES = [['F', 'H'], ['F', 'E']]
+# ~ CLAVES = [['E', 'F'], ['F', 'H']]
 
 # ~ Set5
 # ~ ARGUMENTOS:
 ERE5 = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
 EFE5 = [ (['A'], ['F']), (['A'],['G']), (['B'],['E']), (['C', 'D']), (['D', 'B']), (['E'], ['A']), (['F', 'G'], ['C']) ]
 # ~ RESULTADO OBTENIDO
-# ~ CLAVES = [['A'], ['E'], ['D'], ['C'], ['B'], ['F', 'G']]
+# ~ CLAVES = [['A'], ['B'], ['C'], ['D'], ['E'], ['F', 'G']]
+
+# ~ Set6
+# ~ Set extra que pensamos, que tiene múltiples claves candidatas.
+# ~ ARGUMENTOS:
+ERE6 = ['A', 'B', 'C', 'D', 'E', 'F']
+EFE6 = [ (['B'], ['C']), (['C'],['B']), (['D'],['E', 'F']), (['E','F'],  ['D'])]
+# ~ RESULTADO OBTENIDO
+# ~ CLAVES = [['A', 'B', 'D'], ['A', 'B', 'E', 'F'], ['A', 'C', 'D'], ['A', 'C', 'E', 'F']]
 
 
 
-#Calcula las claves candidatas y las imprime en pantalla	
+#Calcula las claves candidatas y las imprime en pantalla.	
 def main(ERE, EFE, n):
     print("Claves candidatas del Set "+ str(n) +": ", end = "")
     print(clavesCandidatas(ERE, EFE))
@@ -84,7 +110,7 @@ def main(ERE, EFE, n):
 if __name__ == "__main__":
     main(ERE3,EFE3,3)
     main(ERE4,EFE4,4)
-    main(ERE5,EFE5,5)	
-	
+    main(ERE5,EFE5,5)
+    main(ERE6,EFE6,6)
 	
 	
